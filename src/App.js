94f26c9a760bc0561bd4e64e279b98d196912cf2
Jujaray Naveen <jujaraynaveen@gmail.com>
2020-07-16 00:59:46 +0530

    5

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Main from "./containers/Main";
+require('dotenv').config()
 
 function App() {
   return (
+    console.log(process.env.REACT_APP_GITHUB_TOKEN);
     <div>
       <Main />
     </div>
